6a3ee41949d0adfb3fae3bc1a5ce08f3cd17ec2e
PNikitsin <pnikitinbox@gmail.com>
2022-09-03 11:15:41 +0300

    Change ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ConsoleApp
+﻿using MathLibrary;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                Console.Clear();
+
+                Console.Write("Enter expression: ");
+                string expression = Console.ReadLine();
+
+                if (expression == "")
+                {
+                    Console.WriteLine("Missing expression input!");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                try
+                {
+                    int result = Calculator.Calculate(expression);
+                    Console.WriteLine($"The expression result is: {result}");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Division by zero is prohibited!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid expression!");
+                }
+
+                Console.ReadKey();
+            }
         }
     }
 }
