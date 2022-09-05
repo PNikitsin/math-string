48ab9999022b535cd2f4beec4886afb5faf1a5d2
PNikitsin <pnikitinbox@gmail.com>
2022-09-05 21:52:09 +0300

    Add TestCalculator

---
+++ MathString.Tests/TestCalculator.cs
@@ -0,0 +1,26 @@
+﻿using MathLibrary;
+using NUnit.Framework;
+
+namespace MathString.Tests
+{
+    [TestFixture]
+    internal class TestCalculator
+    {
+        [Test]
+        public void DivisionByZero()
+        {
+            string expression = "2 / 0";
+
+            Assert.Throws<DivideByZeroException>(() => Calculator.Calculate(expression));
+        }
+
+        [Test]
+        public void TestOperations()
+        {
+            string expression = "2+2*(222-125)+32/2*1";
+            int result = Calculator.Calculate(expression);
+
+            Assert.That(result, Is.EqualTo(212));
+        }
+    }
+}
